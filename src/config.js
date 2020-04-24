720820c991308052ba0ebe450c9b6840b1e9a872
Michael Große <mic.grosse@googlemail.com>
2020-04-24 14:53:15 +0200

    Make it usable by default without a build step
    A build step makes things a lot more complex and as this commit shows,
    modern browsers can handle module import/export just fine.

---
+++ src/config.js
@@ -0,0 +1,13 @@
+const config = [
+	{
+		x: 1920,
+		y: 1080,
+		url: "https://gist.githubusercontent.com/addshore/e51a9f03decc66d9267f19323d1ac0cf/raw/f0c0aba2e1cc39a2ff14ee14ace81d1e51bac43b/map-20200424-1920-1080-pixels.csv",
+	},
+	// {
+	// 	x: 3840,
+	// 	y: 2160,
+	// }
+];
+
+export default config;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-const dataUrl =
-	"https://gist.githubusercontent.com/addshore/e51a9f03decc66d9267f19323d1ac0cf/raw/f0c0aba2e1cc39a2ff14ee14ace81d1e51bac43b/map-20200424-1920-1080-pixels.csv";
+import config from './config';
+
+const dataUrl =	config[0].url;
 fetch(dataUrl, { mode: "cors" })
 	.then((response) => response.text())
 	.then((data) => {
 		const canvas = document.getElementById("wdmap");
+		canvas.width = config[0].x;
+		canvas.height = config[0].y;
 		const ctx = canvas.getContext("2d");
 		ctx.fillStyle = "black";
 		ctx.fillRect(0, 0, canvas.width, canvas.height);
