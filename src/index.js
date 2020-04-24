4ad86e33eb917cabafabdf9da35a2a0456d20021
Michael Große <mic.grosse@googlemail.com>
2020-04-24 15:20:38 +0200

    Add canvas dynamically
    This will allow us to add and switch more canvases

--- src/index.js
+++ src/index.js
@@ -1,25 +1,42 @@
 import config from './config';
 
-const dataUrl =	config[0].url;
-fetch(dataUrl, { mode: "cors" })
-	.then((response) => response.text())
-	.then((data) => {
-		const canvas = document.getElementById("wdmap");
-		canvas.width = config[0].x;
-		canvas.height = config[0].y;
-		const ctx = canvas.getContext("2d");
-		ctx.fillStyle = "black";
-		ctx.fillRect(0, 0, canvas.width, canvas.height);
+const wdMapCanvases = [];
 
-		const csvLines = data.split("\n");
-		const tStartRender = performance.now();
-		csvLines.forEach(async (line) => {
-			const [x, y, numberOfItems] = line.split(",");
-			const ci = (255 * numberOfItems) / 100;
-			ctx.fillStyle = `rgb(${ci}, ${ci}, ${ci})`;
-			ctx.fillRect(x, y, 1, 1);
+function showDensity(x, y, url) {
+	if (!wdMapCanvases[`${x}x${y}`]) {
+		wdMapCanvases[`${x}x${y}`] = createAndRenderDensityCanvas(x, y, url)
+	}
+	wdMapCanvases.forEach(canvas => canvas.style.display = 'none');
+	wdMapCanvases[`${x}x${y}`].style.display = 'block;'
+}
+
+function createAndRenderDensityCanvas(x, y, url) {
+	const canvas = document.createElement('canvas');
+	document.querySelector('body').appendChild(canvas);
+
+	fetch(url, { mode: "cors" })
+		.then((response) => response.text())
+		.then((data) => {
+			canvas.width = x;
+			canvas.height = y;
+			const ctx = canvas.getContext("2d");
+			ctx.fillStyle = "black";
+			ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+			const csvLines = data.split("\n");
+			const tStartRender = performance.now();
+			csvLines.forEach(async (line) => {
+				const [x, y, numberOfItems] = line.split(",");
+				const ci = (255 * numberOfItems) / 100;
+				ctx.fillStyle = `rgb(${ci}, ${ci}, ${ci})`;
+				ctx.fillRect(x, y, 1, 1);
+			});
+			const tEndRender = performance.now();
+			const perfP = document.getElementById("performance");
+			perfP.textContent = `Render for ${x}x${y} took ${(tEndRender - tStartRender)} milliseconds.`;
 		});
-		const tEndRender = performance.now();
-		const perfP = document.getElementById("performance");
-		perfP.textContent = "Render took " + (tEndRender - tStartRender) + " milliseconds.";
-	});
+
+	return canvas;
+}
+
+showDensity(config[0].x, config[0].y, config[0].url);
