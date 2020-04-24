58b30e78e290e29585b22222cd8eecd0c5b8699f
Michael Große <mic.grosse@googlemail.com>
2020-04-24 15:59:08 +0200

    Show rivers in big resolution

--- src/index.js
+++ src/index.js
@@ -39,7 +39,33 @@ function createAndRenderDensityCanvas(x, y, url) {
 			});
 			const tEndRender = performance.now();
 			const perfP = document.getElementById("performance");
-			perfP.textContent = `Render for ${x}x${y} took ${(tEndRender - tStartRender)} milliseconds.`;
+			perfP.textContent += ` Render for ${x}x${y} took ${(tEndRender - tStartRender)} milliseconds.`;
+
+			if (x > 3000) {
+				const riverUrl = 'https://gist.githubusercontent.com/addshore/01c9aa9c449b8208c1da06010017a469/raw/54fefc91332dcb779abe5b862c262558236dacb5/map-20200424-3840-2160-relations-P403.csv';
+				fetch(riverUrl, { mode: "cors" })
+					.then((response) => response.text())
+					.then((data) => {
+						const ctx = canvas.getContext("2d");
+						const csvLines = data.split("\n");
+
+						const tStartRender = performance.now();
+
+						csvLines.forEach(async (line) => {
+							const [x1, y1, x2, y2] = line.split(",");
+							ctx.strokeStyle = `blue`;
+							ctx.beginPath();
+							ctx.moveTo(x1, y1);
+							ctx.lineTo(x2, y2);
+							ctx.stroke();
+						});
+
+						const tEndRender = performance.now();
+						const perfP = document.getElementById("performance");
+						perfP.textContent += ` Render for rivers took ${(tEndRender - tStartRender)} milliseconds.`;
+					});
+
+			}
 		});
 
 	return canvas;
