13763dc8a97efe83d8dd72c2b4d3ca57bbe7dcbb
Michael Große <mic.grosse@googlemail.com>
2020-04-24 16:07:42 +0200

    Show only short river lines

--- src/index.js
+++ src/index.js
@@ -53,6 +53,10 @@ function createAndRenderDensityCanvas(x, y, url) {
 
 						csvLines.forEach(async (line) => {
 							const [x1, y1, x2, y2] = line.split(",");
+							const distance = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+							if (distance > 300) {
+								return;
+							}
 							ctx.strokeStyle = `blue`;
 							ctx.beginPath();
 							ctx.moveTo(x1, y1);
