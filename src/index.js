03eb5bdec55c6045cae351e7828169cd634ec240
Michael Große <mic.grosse@googlemail.com>
2020-04-24 19:24:22 +0200

    Smooth drawing for the dots
    This now draws the dots much more smoothly and the canvas is visible
    right from the start.
    
    Further benefits in this patch:
    * refactored the drawing functions
    ** they are now much less cluttered and have much less responsibilities
    ** they are extracted into their own file in order to separate concerns
    * switched to a nice color for the dots.
    ** Though not yet perfect, the dark ones are pretty dark right now
    * added a progress bar
    * removed the noise in the console

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import config from './config.js';
+import { drawDots, drawRivers } from './draw.js';
 
 const wdMapCanvases = {};
 
@@ -16,60 +17,50 @@ function showDensity(x, y, url) {
 	wdMapCanvases[resolutionKey].style.display = 'block';
 }
 
+const progressElement = document.getElementById('progress');
+async function updateProgress(current, total) {
+	window.requestAnimationFrame(() => {
+		progressElement.value = Math.ceil( current / total * 100 );
+	});
+}
+
 function createAndRenderDensityCanvas(x, y, url) {
 	const canvas = document.createElement('canvas');
+	canvas.width = x;
+	canvas.height = y;
+	const ctx = canvas.getContext("2d");
+	ctx.fillStyle = "black";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
 	document.querySelector('body').appendChild(canvas);
 
 	fetch(url, { mode: "cors" })
 		.then((response) => response.text())
-		.then((data) => {
-			canvas.width = x;
-			canvas.height = y;
-			const ctx = canvas.getContext("2d");
-			ctx.fillStyle = "black";
-			ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-			const csvLines = data.split("\n");
+		.then(async (data) => {
 			const tStartRender = performance.now();
-			csvLines.forEach(async (line) => {
-				const [x, y, numberOfItems] = line.split(",");
-				const ci = (255 * numberOfItems) / 100;
-				ctx.fillStyle = `rgb(${ci}, ${ci}, ${ci})`;
-				ctx.fillRect(x, y, 1, 1);
-			});
+
+			await drawDots(data, ctx, updateProgress);
+
 			const tEndRender = performance.now();
 			const perfP = document.getElementById("performance");
-			perfP.textContent += ` Render for ${x}x${y} took ${(tEndRender - tStartRender)} milliseconds.`;
-
-			if (x < 3000) {
-				const riverUrl = 'https://gist.githubusercontent.com/addshore/01c9aa9c449b8208c1da06010017a469/raw/54fefc91332dcb779abe5b862c262558236dacb5/map-20200424-3840-2160-relations-P403.csv';
-				fetch(riverUrl, { mode: "cors" })
-					.then((response) => response.text())
-					.then((data) => {
-						const ctx = canvas.getContext("2d");
-						const csvLines = data.split("\n");
-
-						const tStartRender = performance.now();
-
-						csvLines.forEach(async (line) => {
-							const [x1, y1, x2, y2] = line.split(",");
-							const distance = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
-							if (distance > 300) {
-								return;
-							}
-							ctx.strokeStyle = `blue`;
-							ctx.beginPath();
-							ctx.moveTo(x1, y1);
-							ctx.lineTo(x2, y2);
-							ctx.stroke();
-						});
-
-						const tEndRender = performance.now();
-						const perfP = document.getElementById("performance");
-						perfP.textContent += ` Render for rivers took ${(tEndRender - tStartRender)} milliseconds.`;
-					});
-
+			const key = `${x}x${y}`;
+			perfP.textContent += ` Rendering ${key} took ${Math.ceil(tEndRender - tStartRender)} milliseconds.`;
+		})
+		.then(() => {
+			if (x > 3000) {
+				return;
 			}
+			const riverUrl = 'https://gist.githubusercontent.com/addshore/01c9aa9c449b8208c1da06010017a469/raw/54fefc91332dcb779abe5b862c262558236dacb5/map-20200424-3840-2160-relations-P403.csv';
+			return fetch(riverUrl, { mode: "cors" });
+		})
+		.then((response) => response.text())
+		.then(async (data) => {
+			const tStartRender = performance.now();
+
+			await drawRivers(data, ctx);
+
+			const tEndRender = performance.now();
+			const perfP = document.getElementById( 'performance' );
+			perfP.textContent += ` Rendering rivers took ${( tEndRender - tStartRender )} milliseconds.`;
 		});
 
 	return canvas;
@@ -79,7 +70,6 @@ const form = document.getElementById('resolutionSelector');
 
 function updateCanvas() {
 	const index = form.querySelector('input[name="resolution"]:checked').value;
-	console.log(`switching to ${index}`);
 	showDensity(config[index].x, config[index].y, config[index].url);
 }
 
