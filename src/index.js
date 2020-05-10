36eb0af103b0b3450e55d89a49e3412df9f815f1
addshore <adamshorland@gmail.com>
2020-05-10 10:10:30 +0100

    canvases, divide dot and river canvas & draw to correct res
    This splits up the river and the main map canvas so that if the river
    stuff ends up drawing before the dots, the dots dont then overwrite the
    rivers.
    This also makes sure that the worker writes to the correct canvas if the
    user switches away mid render.
    
    Multiple canvas layers also means that we can start rendering each layer
    at the same time (thanks streams and service workers).
    This will be grent when we also have even more layers!
    
    This also now means that you can't right click download the map, as the
    reiver canvas is in front.
    This change also changes the transparancy of the river drawings so they
    are less in your face.

--- src/index.js
+++ src/index.js
@@ -1,55 +1,65 @@
 import config from './config.js';
 import { drawDot, drawLine } from './draw.js';
 
-const wdMapCanvases = {};
 let worker = new Worker('./../src/worker.js');
+
+const wdMapCanvases = {};
 
 function showDensity(x, y, url, riverUrl) {
 	const resolutionKey = `${x}x${y}`;
 	if (!wdMapCanvases[resolutionKey]) {
-		wdMapCanvases[resolutionKey] = createAndRenderDensityCanvas(x, y, url, riverUrl)
+		wdMapCanvases[resolutionKey] = createAndRenderCanvases(resolutionKey, x, y, url, riverUrl)
+		document.querySelector('#canvas-container').appendChild(wdMapCanvases[resolutionKey][0]);
+		document.querySelector('#canvas-container').appendChild(wdMapCanvases[resolutionKey][1]);
 	}
 
 	for (const resolution in wdMapCanvases) {
-		const canvas = wdMapCanvases[resolution];
-		canvas.style.display = 'none';
+		const canvases = wdMapCanvases[resolution];
+		canvases[0].style.display = 'none';
+		canvases[1].style.display = 'none';
 	}
 
-	wdMapCanvases[resolutionKey].style.display = 'block';
+	wdMapCanvases[resolutionKey][0].style.display = 'block';
+	wdMapCanvases[resolutionKey][1].style.display = 'block';
+
 }
 
-const progressElement = document.getElementById('progress');
-async function updateProgress(current, total) {
-	window.requestAnimationFrame(() => {
-		progressElement.value = Math.ceil( current / total * 100 );
-	});
-}
-
-function createAndRenderDensityCanvas(x, y, url, riverUrl) {
+function newCanvas(x, y, fillStyle){
 	const canvas = document.createElement('canvas');
 	canvas.width = x;
 	canvas.height = y;
+	canvas.style= "position: absolute; left: 0; top: 0; z-index: 0;";
 	const ctx = canvas.getContext("2d");
-	ctx.fillStyle = "black";
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
-	document.querySelector('body').appendChild(canvas);
+	if(fillStyle === 'clear') {
+		ctx.clearRect(0, 0, canvas.width, canvas.height)
+	} else {
+		ctx.fillStyle = fillStyle;
+		ctx.fillRect(0, 0, canvas.width, canvas.height);
+	}
+	return canvas
+}
 
+function createAndRenderCanvases(resolutionKeyToRender, x, y, url, riverUrl) {
+	const mainCanvas = newCanvas(x, y, 'black');
+	const riverCanvas = newCanvas(x, y, 'clear');
 	worker.onmessage = function(e) {
-		const [drawType, batchedValues] = e.data;
+		const [resolutionKey, drawType, batchedValues] = e.data;
+		if(resolutionKeyToRender !== resolutionKey) {
+			return;
+		}
 		window.requestAnimationFrame( function() {
 			batchedValues.forEach( function(drawData){
 				if(drawType === 'dot') {
-					drawDot(ctx, drawData)
+					drawDot(mainCanvas.getContext("2d"), drawData)
 				}
 				if(drawType === 'line') {
-					drawLine(ctx, drawData)
-
+					drawLine(riverCanvas.getContext("2d"), drawData)
 				}
 			} )
 		} )
 	}
-	worker.postMessage([url, riverUrl])
-	return canvas;
+	worker.postMessage([resolutionKeyToRender, url, riverUrl])
+	return [mainCanvas, riverCanvas];
 }
 
 const form = document.getElementById('resolutionSelector');
