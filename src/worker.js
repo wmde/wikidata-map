38880a4089d4ba1d1fbbf7f382c0589d841d860f
addshore <git@addshore.com>
2025-10-28 22:06:43 +0000

    Rework the UI, for a much nicer feel

--- src/worker.js
+++ src/worker.js
@@ -8,16 +8,16 @@ self.importScripts(
  * - Use stream processing to decode it and batch it up (see stream.js)
  * - Send it back to the main thread to render
  */
-self.startFetch = async function( layerKey, dateIndex, intensityScale, propertyLayerConfig ) {
-	console.log("Worker: Rendering canvas " + dateIndex + " " + layerKey)
+self.startFetch = async function( layerKey, dateStr, intensityScale, propertyLayerConfig ) {
+	console.log("Worker: Rendering canvas " + dateStr + " " + layerKey)
 	if(layerKey.lastIndexOf("items", 0) === 0) {
-		let mapUrl = 'https://analytics.wikimedia.org/published/datasets/one-off/wikidata/wmde_wikidata_map/map-' + dateIndex + '-7680-4320-pixels.csv'
+		let mapUrl = 'https://analytics.wikimedia.org/published/datasets/one-off/wikidata/wmde_wikidata_map/map-' + dateStr + '-7680-4320-pixels.csv'
 		await fetch(mapUrl, { mode: "cors" })
 		.then(response => {return chunksToLinesReadableStream( response.body.getReader() )})
 		.then(dataStream => {return valuesToBatchedValuesReadableStream( dataStream.getReader(), 5000 )})
 		.then(batchedStream => {return batchedToWorkerMessageReadableStream( batchedStream.getReader(), postMessage, {
 			layerKey: layerKey,
-			dateIndex: dateIndex,
+			dateStr: dateStr,
 			drawType: 'dot',
 			intensityScale: intensityScale,
 		} )})
@@ -26,14 +26,14 @@ self.startFetch = async function( layerKey, dateIndex, intensityScale, propertyL
 		propertyLayerConfig.forEach( function(layer){
 			let propertyId = layer.id;
 			if(propertyId == layerKey) {
-				let layerUrl = 'https://analytics.wikimedia.org/published/datasets/one-off/wikidata/wmde_wikidata_map/map-' + dateIndex + '-7680-4320-relation-pixels-{property}.csv'
+				let layerUrl = 'https://analytics.wikimedia.org/published/datasets/one-off/wikidata/wmde_wikidata_map/map-' + dateStr + '-7680-4320-relation-pixels-{property}.csv'
 				layerUrl = layerUrl.replace('{property}', propertyId);
 				fetch(layerUrl, { mode: "cors" })
 					.then(response => {return chunksToLinesReadableStream( response.body.getReader() )})
 					.then(dataStream => {return valuesToBatchedValuesReadableStream( dataStream.getReader(), 5000 )})
 					.then(batchedStream => {return batchedToWorkerMessageReadableStream( batchedStream.getReader(), postMessage, {
 						layerKey: layerKey,
-						dateIndex: dateIndex,
+						dateStr: dateStr,
 						drawType: propertyId,
 						intensityScale: intensityScale,
 						lineMaxPercent: layer.lineMaxPercent,
@@ -46,7 +46,7 @@ self.startFetch = async function( layerKey, dateIndex, intensityScale, propertyL
 }
 
 onmessage = function(e) {
-	const [layerKey, dateIndex, intensityScale, propertyLayerConfig] = e.data;
-	console.log('Worker: Message to render canvas ' + dateIndex + " " + layerKey)
-	self.startFetch( layerKey, dateIndex, intensityScale, propertyLayerConfig )
+	const [layerKey, dateStr, intensityScale, propertyLayerConfig] = e.data;
+	console.log('Worker: Message to render canvas ' + dateStr + " " + layerKey)
+	self.startFetch( layerKey, dateStr, intensityScale, propertyLayerConfig )
 }
